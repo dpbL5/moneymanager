dacbe0190c9e286f88fbe908e2f18772171f6469
Duong Phan Bao Linh <linhdpb.b22cn485@stu.ptit.edu.vn>
2024-10-24 23:42:06 +0700

    Change ifAccount Directory

--- src/main/java/oop/moneymanager/App.java
+++ src/main/java/oop/moneymanager/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,7 +16,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ifAccount.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("User/ifAccount.fxml"));
         primaryStage.setTitle("Money Manager");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
