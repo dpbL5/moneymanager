08faa2ed3a601a5a2e6cd98c1cfd1868e6966bb4
tcnguyen <nguyentruong260804@gmail.com>
2024-11-21 08:39:21 +0700

    update completely

--- src/main/java/oop/moneymanager/dao/JDBCUtil.java
+++ src/main/java/oop/moneymanager/dao/JDBCUtil.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class  JDBCUtil {
     private static String url = "jdbc:mysql://localhost:3306/moneymanager?autoReconnect=true&useSSL=false";
     private static String userName = "root";
-    private static String passWord = "baolinh56";
+    private static String passWord = "root";
     public static Connection getConnection(){
         Connection con = null;
         try {
